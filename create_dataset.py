a9d1b11a312f016ee3ffed9c62f7af4200825f80
Mike <michael.birkholz@estudiantat.upc.edu>
2021-12-19 21:33:35 +0100

    Command-line args

--- create_dataset.py
+++ create_dataset.py
@@ -10,6 +10,7 @@
 #   - MPI-INF-3DHP
 
 
+import argparse
 import json
 import numpy as np
 
@@ -23,6 +24,30 @@ from py3dpw import Py3DPW
 #from pymi3 import PyMI3
 
 from utils import GID
+
+# Command-line arguments
+ap=argparse.ArgumentParser()
+ap.add_argument("-m", "--mpii", action='store_true', help='Use MPII dataset')
+ap.add_argument("-l", "--lsp", action='store_true', help='Use LSP dataset')
+ap.add_argument("-e", "--lspet", action='store_true', help='Use LSPET dataset')
+ap.add_argument("-c", "--coco", action='store_true', help='Use COCO dataset')
+ap.add_argument("-p", "--3dpw", action='store_true', help='Use 3DPW dataset')
+ap.add_argument("-f", "--3dhp", action='store_true', help='Use MPI-INF-3DHP dataset')
+ap.add_argument("-s", "--set", required=True, help="Set: train, val, test or toy")
+args=vars(ap.parse_args())
+
+# If no dataset is specified, generate them all
+USE_MPII=args['mpii']
+USE_LSP=args['lsp']
+USE_LSPET=args['lspet']
+USE_COCO=args['coco']
+USE_3DPW=args['3dpw']
+USE_3DHP=args['3dhp']
+if not (USE_MPII or USE_LSP or USE_LSPET or USE_COCO or USE_3DPW or USE_3DHP):
+    USE_MPII=USE_LSP=USE_LSPET=USE_COCO=USE_3DPW=USE_3DHP=True
+
+which_set = args['set']
+assert(which_set in ['train','val','test','toy'])
 
 # Suppress scientific notation in numpy prints (really annoying for pixel locations)
 np.set_printoptions(suppress=True)
@@ -56,22 +81,25 @@ dataset = {}
 #mpii_data = mpii.gather_data('toy', gid=gid)  # TODO: Remove
 #coco_data = coco.gather_data('toy', gid=gid)
 
-which_set='test'  # 'train', 'test', 'val', 'toy'
-lsp_data = lsp.gather_data(which_set, gid=gid)
-lspet_data = lspet.gather_data(which_set, gid=gid)
-mpii_data = mpii.gather_data(which_set, gid=gid)
-coco_data = coco.gather_data(which_set, gid=gid)
-tdpw_data = tdpw.gather_data(which_set, gid=gid)
-
-mi3_data = [ "no data either" ]
-
-# Add data from each dataset to top-level dict
-dataset['lsp']=lsp_data
-dataset['lspet']=lspet_data
-dataset['mpii']=mpii_data
-dataset['coco']=coco_data
-dataset['3dpw']=tdpw_data
-dataset['mpi-inf-3dhp']=mi3_data
+# Gather data from each enabled dataset and add to top-level dict
+if USE_LSP:
+    lsp_data = lsp.gather_data(which_set, gid=gid)
+    dataset['lsp']=lsp_data
+if USE_LSPET:
+    lspet_data = lspet.gather_data(which_set, gid=gid)
+    dataset['lspet']=lspet_data
+if USE_MPII:
+    mpii_data = mpii.gather_data(which_set, gid=gid)
+    dataset['mpii']=mpii_data
+if USE_COCO:
+    coco_data = coco.gather_data(which_set, gid=gid)
+    dataset['coco']=coco_data
+if USE_3DPW:
+    tdpw_data = tdpw.gather_data(which_set, gid=gid)
+    dataset['3dpw']=tdpw_data
+if USE_3DHP:
+    mi3_data = [ "no data" ]
+    dataset['mpi-inf-3dhp']=mi3_data
 
 # Write to file
 with open('dataset.json', 'w') as outfile:
