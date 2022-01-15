d3b3ad1d963029886e2655c02599f6e4bff9a960
Mike <michael.birkholz@estudiantat.upc.edu>
2022-01-15 11:14:30 +0000

    Silhouette generation for MPI-INF-3DHP, first attempt

--- preprocessing/maskrcnn/silhouette.py
+++ preprocessing/maskrcnn/silhouette.py
@@ -52,18 +52,23 @@ def valid_joints(jointsx, jointsy, dims):
 
 def joint_bbox(jointsx, jointsy, dims):
     joint_mask=valid_joints(jointsx, jointsy, dims)
-    c0=int(jointsx[joint_mask].min())
-    c1=int(jointsx[joint_mask].max())
-    r0=int(jointsy[joint_mask].min())
-    r1=int(jointsy[joint_mask].max())
-    print(f'joint_bbox: {r0} {c0} {r1} {c1}')
+    if joint_mask.any() == True:
+        c0=int(jointsx[joint_mask].min())
+        c1=int(jointsx[joint_mask].max())
+        r0=int(jointsy[joint_mask].min())
+        r1=int(jointsy[joint_mask].max())
+    else:
+        c0=c1=r0=r1=0
     return (r0, c0, r1, c1)
 
 def eval_joints_mask(mask, jointsx, jointsy):
+    score=0
     joint_mask=valid_joints(jointsx, jointsy, mask.shape)
     valjx=jointsx[joint_mask]
     valjy=jointsy[joint_mask]
-    return np.mean([mask[int(jy),int(jx)] for jx, jy in zip(valjx, valjy)])
+    if joint_mask.any()==True:
+        score = np.mean([mask[int(jy),int(jx)] for jx, jy in zip(valjx, valjy)])
+    return score
 
 def bbox_iou(box1, box2):
     iou=0
@@ -83,16 +88,10 @@ def bbox_iou(box1, box2):
     if intersection>0:
         union=((box1[2]-box1[0])*(box1[3]-box1[1])+(box2[2]-box2[0])*(box2[3]-box2[1]))-intersection
         iou=intersection/union
-        print(f'I: {intersection}')
-        print(f'U: {union}')
-        print(f'IoU: {iou}')
-    else:
-        print(f'IoU: 0')
 
     return iou
 
 def eval_bbox_mask(mask, roi, jointsx, jointsy):
-    print('ROI: ' + str(roi))
     rmin=0
     rmax=mask.shape[0]
     cmin=0
@@ -109,14 +108,7 @@ def eval_bbox_mask(mask, roi, jointsx, jointsy):
     c1=max(roi[3], cmin)
     c1=min(c1, cmax)
 
-    print(f'{rmin} {rmax} {cmin} {cmax}')
-    print(f'{roi[0]} {roi[1]} {roi[2]} {roi[3]}')
-    print(f'{r0} {c0} {r1} {c1}')
-
-    print('mask: ' + str(mask.shape))
-    print('roi: ' + str(mask[r0:r1, c0:c1].shape))
     score = np.sum(mask[r0:r1, c0:c1])/((r1-r0)*(c1-c0))
-    print('score: ' + str(score))
 
     jbb=joint_bbox(jointsx,jointsy,mask.shape)
     iou=bbox_iou((r0, c0, r1, c1), jbb)
@@ -138,24 +130,21 @@ def eval_bbox_mask(mask, roi, jointsx, jointsy):
 
 
 def choose_masks(seq, frame, masks, rois, class_ids, scores, class_idx_person):
-    print('choose_masks')
-    print('Sequence/Frame: ' + seq['sequence'] + '/' + str(frame))
-    print('Total masks: ' + str(masks.shape[2]))
     # First, only consider masks tagged as "person"
     people_idxs=(class_ids==class_idx_person)
-    print('people_idxs: ' + str(people_idxs))
-    frame_idx=seq['img_frame_ids'][frame]
-    print('frame_idx: ' + str(frame_idx))
     num_actors=len(seq['poses2d'])
-    print('Num actors: ' + str(num_actors))
-    print('ROIs: ' + str(rois.shape))
+
+    if masks.shape[2] < num_actors:
+        # If we didn't detect at least as many objects as actors, exit
+        # Note: Could be improved to still return 1 if we can figure out which
+        return None
 
     # Create arrays to hold scores
     jscore=np.zeros((num_actors, masks.shape[2]))
     bbscore=np.zeros((num_actors, masks.shape[2]))
 
     for a in range(num_actors):
-        joints=seq['poses2d'][a][frame_idx]
+        joints=seq['poses2d'][a][frame]
         jointsx=joints[0]
         jointsy=joints[1]
         jointsv=joints[2]
@@ -163,11 +152,7 @@ def choose_masks(seq, frame, masks, rois, class_ids, scores, class_idx_person):
             mask=masks[:,:,midx]
             roi=rois[midx,:]
             
-            print(mask.shape)
-            print(jointsx)
-            print(jointsy)
             jscore[a,midx]=eval_joints_mask(mask, jointsx, jointsy)
-            print('jpoints: ' + str(jscore[a,midx]))
             bbscore[a,midx]=eval_bbox_mask(mask,roi,jointsx,jointsy) 
             
             masked_image = np.zeros_like(mask, dtype=np.uint8)
@@ -179,23 +164,21 @@ def choose_masks(seq, frame, masks, rois, class_ids, scores, class_idx_person):
             #ax.plot(jointsx[valj],jointsy[valj],'ro')
             #plt.show()
     # Check the scores and pick best match for each actor
-    print('JSCORE')
-    print(jscore)
-    print('BBSCORE')
-    print(bbscore)
     best_jscores=np.argmax(jscore, axis=1)
     best_bbscores=np.argmax(bbscore, axis=1)
     if( not np.array_equal(best_jscores, best_bbscores) ):
-        print('WARNING! Not equal!')
+        print(f"WARNING! J/BB scores not equal!' Seq: {seq['sequence']} Frame: {frame}")
         print('J Scores: ' + str(best_jscores))
+        print(jscore)
         print('BB Scores: ' + str(best_bbscores))
+        print(bbscore)
 
     # Additionally, make sure best match is a person, not some other class
     for i in range(len(best_jscores)):
         if not people_idxs[best_jscores[i]]:
-            print('J Score not human!')
+            print(f"J score not human! Seq: {seq['sequence']} Frame: {frame}")
         if not people_idxs[best_jscores[i]]:
-            print('BB Score not human!')
+            print(f"BB score not human! Seq: {seq['sequence']} Frame: {frame}")
 
     return best_bbscores
 
@@ -221,6 +204,9 @@ dirs_3dpw = sorted(next(os.walk(base_img_dir))[1])
 # join dirs
 for d in dirs_3dpw:
     curdir=os.path.join(base_img_dir, d)
+    if os.path.split(curdir)[1]=='downtown_cafe_01':
+        # Unfortunately, this pkl is missing from the data
+        continue
     img_list=sorted(next(os.walk(curdir))[2])
     for img in img_list:
         all_3dpw_imgs.append(os.path.join(TDPW_IMG_DIR, d, img))
@@ -242,15 +228,13 @@ for d, p_list in zip([f'{BTFM_BASE}{TDPW_TRAIN_DIR}', f'{BTFM_BASE}{TDPW_VAL_DIR
         sequences[p[:-4]] = pickle.load(inpickle,encoding='latin1')
         inpickle.close()
 
-# TODO: Filter out only the (up to 2) silhouettes we care about.
-#       Based on class==human and ??? (maybe bbox/sil and joint overlap?)
+images_processed=0
 octr=0
 for d in dirs_3dpw:
     if d=='downtown_cafe_01':
         # Unfortunately, this pkl is missing from the data
         continue
     ictr=0
-    dir_results = {}
     os.mkdir(f'{BTFM_BASE}{BTFM_PP_SILHOUETTE}/{d}')
     curdir=os.path.join(base_img_dir, d)
     img_list=sorted(next(os.walk(curdir))[2])
@@ -258,8 +242,6 @@ for d in dirs_3dpw:
         curimg=skimage.io.imread(os.path.join(curdir, img))
         results=model.detect([curimg], verbose=0)
         r=results[0]
-        print('Num masks: ' + str(len(r['masks'])))
-        print('Shape masks: ' + str(r['masks'].shape))
 
         # Use metadata to decide best silhouettes for each actor
         seq_key=d
@@ -267,82 +249,22 @@ for d in dirs_3dpw:
         best_masks = choose_masks(sequences[seq_key], frame, r['masks'], r['rois'],
             r['class_ids'], r['scores'], class_idx_person)
 
-#visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
-#                            class_names, r['scores'])
-
-        for a, m in enumerate(best_masks):
-            save_silhouette(f'{BTFM_BASE}{BTFM_PP_SILHOUETTE}/{d}/{img[:-4]}_subj{a}.png',r['masks'][:,:,m])
+        if best_masks is not None:
+            # Note: If choose_masks is updated to return only a single mask, need to update this too
+            for a, m in enumerate(best_masks):
+                save_silhouette(f'{BTFM_BASE}{BTFM_PP_SILHOUETTE}/{d}/{img[:-4]}_subj{a}.png',r['masks'][:,:,m])
             
         #for m in range(r['masks'].shape[2]):
         #    save_silhouette(f'{BTFM_BASE}{BTFM_PP_SILHOUETTE}/{d}/{img[:-4]}_{m}.png',r['masks'][:,:,m])
-        dir_results[os.path.join(TDPW_IMG_DIR, d, img)]=results
+        images_processed+=1
         ictr+=1
-        if ictr == 10:
-            break
+        if 0 == (ictr%100):
+            print(f'Images processed: {images_processed}/{total_imgs}')
 
-
-    outfile=open(BTFM_BASE+BTFM_PP_SILHOUETTE+'/'+d+'.pkl', 'wb')
-    pickle.dump(dir_results, outfile)
-    outfile.close()
-
+    print(f'Images processed: {images_processed}/{total_imgs}')
     octr+=1
     if octr==3:
         #break
         pass
 
-exit()
-
-for i in [0]:
-    batch_start=i*batch_size
-    batch = [skimage.io.imread(x) for x in all_3dpw_imgs[batch_start:batch_start+batch_size]]
-    start_time=time.time()
-    results = model.detect(batch, verbose=0)
-    elapsed=time.time()-start_time
-    pct = 100*(i+1)*batch_size/len(all_3dpw_imgs)
-    print('Progress: ' + str(pct) + '%')
-    print('Time per image: ' + str(elapsed/batch_size))
-
-    # TODO: Put this code in handle_result
-
-    batch_filenames=[x for x in all_3dpw_imgs[batch_start:batch_start+batch_size]]
-    for fn,r,image in zip(batch_filenames,results,batch):
-        print(fn)
-        num_people=np.sum(np.array(r['class_ids'])==class_idx_person)
-        print('People: ' + str(num_people))
-        visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
-                                    class_names, r['scores'])
-        
-#if(0==(i%10)):
-#    pct = 100*i/len(all_3dpw_imgs)
-#    print('Progress: ' + str(pct) + '%')
-
-
-
 print('Done.')
-
-#for i, img_fn in enumerate(all_3dpw_imgs):
-#    image = skimage.io.imread(img_fn)
-#    results = model.detect([image], verbose=0)
-#    if(0==(i%100)):
-#        pct = 100*i/len(all_3dpw_imgs)
-#        print('Progress: ' + str(pct) + '%')
-
-#print(next(os.walk(IMAGE_DIR)))
-
-## Load a random image from the images folder
-#print(next(os.walk(IMAGE_DIR)))
-#file_names = next(os.walk(IMAGE_DIR))[2]
-#print("PRINTING STUFF!")
-#print(IMAGE_DIR)
-#print(file_names)
-#image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
-#
-## Run detection
-#results = model.detect([image], verbose=1)
-#
-## Visualize results
-#r = results[0]
-#print(r)
-#visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
-#                            class_names, r['scores'])
-
